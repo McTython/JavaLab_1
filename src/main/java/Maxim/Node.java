cc8d4dc334a221fedd6fe5b4010f463dc8d0726d
Maxim <mrmob67@gmail.com>
2024-03-19 16:47:11 +0300

    add setValue method

--- src/main/java/Maxim/Node.java
+++ src/main/java/Maxim/Node.java
@@ -9,15 +9,20 @@ class Node {
         this.next = null;
     }
 
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+
     public int getValue() {
         return this.value;
     }
 
-    public Node getNext() {
-        return this.next;
-    }
-
     public void setNext(Node next) {
         this.next = next;
+    }
+
+    public Node getNext() {
+        return this.next;
     }
 }
